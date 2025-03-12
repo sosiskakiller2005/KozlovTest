765ff9541a4e5e0874fe083e6dd99875be43d76c
Козлов.Владислав <Vkozlov15012005@gmail.com>
2025-03-12 12:16:52 +0300

    Добавьте файлы проекта.

---
+++ Views/Pages/StartPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using KozlovTest.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KozlovTest.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TeacherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FrameHelper.selectedFrame.Navigate(new PasswordPage());
+        }
+
+        private void Student_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
